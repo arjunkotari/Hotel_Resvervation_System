23ae56bf9e5828d88c26328285924521decedfa6
arjunkotari <arjun.kotari123@gmail.com>
2021-08-09 09:21:04 +0530

    Added:Hotel details

---
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class Hotel {
+    public String hotelName;
+    public int HotelRateRegularWeekDay;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+    public int getHotelRateRegularWeekDay() {
+        return HotelRateRegularWeekDay;
+    }
+    public void setHotelRateRegularWeekDay(int HotelRateRegularWeekDay) {
+        this.HotelRateRegularWeekDay = HotelRateRegularWeekDay;
+    }
+
+    public Hotel(String hotelName, int HotelRateRegularWeekDay) {
+        super();
+        this.hotelName = hotelName;
+        this.HotelRateRegularWeekDay = HotelRateRegularWeekDay;
+
+    }
+}
--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,7 +1,41 @@
 package com.bridgelabz;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HotelReservationSystem {
+    /*
+    Hotel Reservation System
+     */
+    //Map for storing details
+    private static Map<String, Hotel> hotelMap;
+    public HotelReservationSystem() {
+        hotelMap = new HashMap<>();
+    }
+    /*
+    Adding hotel details
+    */
+    public boolean addHotel(String name, int HotelRateRegularWeekdays) {
+        Hotel hotelObject = new Hotel(name, HotelRateRegularWeekdays);
+        hotelMap.put(name, hotelObject);
+        return true;
+    }
+
+    /*
+    Printing hotel details
+     */
+    public void printHotels() {
+        for (Map.Entry<String, Hotel> entry : hotelMap.entrySet()) {
+            System.out.println("Hotel Name : " + entry.getKey());
+            System.out.println("Rate on weekdays for regular customers : " + entry.getValue().getHotelRateRegularWeekDay());
+        }
+    }
+    /*
+    Main starts
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System...");
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        hotelReservationSystem.printHotels();
     }
 }
